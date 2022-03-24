def0f9d8f4548a1cb559b80c9719165fb6c66728
A13x4nd7a <alex@alexfaller.uk>
2022-03-24 22:03:19 +0000

    setting up basic first route

--- services/server/src/index.js
+++ services/server/src/index.js
@@ -4,24 +4,29 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const url = 'https://www.virtualrunneruk.com/races/?rfm='
-axios(url)
-.then(response => {
-    const html = response.data
-    const src = cheerio.load(html)
-    const articles = []
-src('.race-item', html).each(function() {
-   const title = src(this).find('.card--race__title').text()
-    const url = src(this).find('a').attr('href')
-    articles.push({
-        title,
-        url
-    })
-    console.log(articles)
- }).catch(err => {
-     console.log(err)
- }
-)
+
+app.get('/', (req, res) => {
+    res.json("Hello World")
 })
+// axios(url)
+// .then(response => {
+//     const html = response.data
+//     const src = cheerio.load(html)
+//     const articles = []
+// src('.race-item', html).each(function() {
+//     const title = src(this).find('.card--race__title').text()
+//     const url = src(this).find('a').attr('href')
+//     articles.push({
+//         title,
+//         url
+//     })
+  
+// }).catch(err => {
+// console.log(err)
+// })
+
+
+// })
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
